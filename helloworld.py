85cf7f16acb476f9b583c3bdf6997991815fa3b2
Joe Trigg <jtrigg17@googlemail.com>
2024-03-11 22:36:00 +0000

    added input to check if user has heard of git

--- helloworld.py
+++ helloworld.py
@@ -10,6 +10,17 @@ else:
     # If name has input then print name.
     print(f"\nHello {name}")
 
+git = input("\nHave you heard of git (y/n): ")
 
 
-print("\nGIT IS AWESOME")
+if git == "y":
+    print("\nSo you know git is awesome")
+
+elif git == "n":
+    print("\nGIT IS AWESOME")
+
+else:
+    print("\nYou didnt input correctly")
+    print("\nYou dont get to know about git")
+
+print("\nGOODBYE")

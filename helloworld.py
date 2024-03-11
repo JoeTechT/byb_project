9a6997813959f2f5f73ec9124a2ce49fff473d2f
Joe Trigg <jtrigg17@googlemail.com>
2024-03-11 22:17:58 +0000

    added user input to find name

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,11 @@
-print("Hello World")
 
-print("GIT IS AWESOME")
+name = input("Please Enter name: ")
+
+if name == "":
+    print("\nHello World")
+else:
+    print(f"\nHello {name}")
+
+
+
+print("\nGIT IS AWESOME")

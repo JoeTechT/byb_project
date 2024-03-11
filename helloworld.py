ad6f74b41eb8fea85f309e1b27bea47b30aedc65
Joe Trigg <jtrigg17@googlemail.com>
2024-03-11 22:57:34 +0000

    Added comments to improve readability

--- helloworld.py
+++ helloworld.py
@@ -10,6 +10,21 @@ else:
     # If name has input then print name.
     print(f"\nHello {name}")
 
+# Gets either a y or n.
+git = input("\nHave you heard of git: ")
+
+if git == "y":
+    # If y print this.
+    print("\nOh so you know git is awesome.")
+
+elif git == "n":
+    print("\nGIT IS AWESOME")
+
+else:
+    # If input is incorret print this.
+    print("\nIncorrect input")
+    print("\nYou dont deserve to know about git.")
 
 
-print("\nGIT IS AWESOME")
+
+print("\nGOODBYE")

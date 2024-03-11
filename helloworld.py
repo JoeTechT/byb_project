144cfc6af0459496ff5d99585d03f94b0e8ed0f8
Joe Trigg <jtrigg17@googlemail.com>
2024-03-11 22:22:27 +0000

    Added comments to code

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,13 @@
 
+# Gathers users name.
 name = input("Please Enter name: ")
 
+# Checks to see if there was an input.
 if name == "":
     print("\nHello World")
 else:
+
+    # If name has input then print name.
     print(f"\nHello {name}")
 
 
